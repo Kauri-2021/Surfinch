81dfef8a1332a209bff4213ba179b5d7305f5ecb
ajpetrizza <ajpetrizza@fullsail.edu>
2021-08-28 15:36:56 -0400

    initial profile setup

---
+++ client/components/Profile/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Profile = (props) => {
+  return (
+    <div className='ProfileParent container'>
+      <div className='row'>
+        <div className='col-6'>
+          THIS IS THE PROFILE INFO VIEW
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  );
+}

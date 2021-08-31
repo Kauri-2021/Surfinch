2589e2d8ecbca5d1a88165555e7e3fcff8db2e0b
ajpetrizza <ajpetrizza@fullsail.edu>
2021-08-31 11:05:13 -0400

    structure is there and now need to build queries

--- client/components/Profile/Profile.jsx
+++ client/components/Profile/Profile.jsx
@@ -1,16 +1,44 @@
 import React from 'react';
+import Avatar from '../Shared/Avatar.jsx';
 
 const Profile = (props) => {
-  return (
-    <div className='ProfileParent container'>
-      <div className='row'>
-        <div className='col-6'>
-          THIS IS THE PROFILE INFO VIEW
-        </div>
-        <div>
 
+  var fakeData = [{ pic: 'URL', name: 'Parrot' }, { pic: 'URL', name: 'Crane' }, { pic: 'URL', name: 'Eagle' }];
+
+  /// HOOKS WILL BE HERE FOR DB CALLS
+
+  return (
+    <div className="profile-parent">
+      <div className='profile-section'>
+        <h3>Profile info:</h3>
+        <div className="profile-info">
+          <Avatar size={75} color='blue' />
+          <div className='user-info container'>
+
+          </div>
+        </div>
+      </div>
+      <div className='social-section'>
+        <h3>Social stats:</h3>
+        <div className="social-view">
+          <h2>Need to decide what stats matter here</h2>
+        </div>
+      </div>
+      <div className='bird-nest-section'>
+        <h3>Bird Nest:</h3>
+        <div className="bird-nest">
+          {fakeData.map((bird, i) => {
+            return (
+              <div className='bird-nest-item'>
+                <Avatar size={75} pic={bird.pic} name={bird.name} color='red' />
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
   );
 }
+
+export default Profile;
+
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,17 @@ module.exports = {
             presets: ['@babel/preset-react', '@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            }
+          }
+        ]
       }
     ]
   }

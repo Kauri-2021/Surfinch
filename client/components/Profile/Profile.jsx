f3bd41e41d236818c0236511e288773872159732
ajpetrizza <ajpetrizza@fullsail.edu>
2021-09-01 11:26:22 -0400

    profile has some of the ideal features for presenting

--- client/components/Profile/Profile.jsx
+++ client/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import Avatar from '../Shared/Avatar.jsx';
 
 const Profile = (props) => {
 
-  var fakeData = [{ pic: 'URL', name: 'Parrot' }, { pic: 'URL', name: 'Crane' }, { pic: 'URL', name: 'Eagle' }];
+  var fakeData = [{ pic: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.ZJormN7TrFZCjK9dnVVyLwHaE8%26pid%3DApi&f=1', name: 'Parrot' }, { pic: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.e_s_eMqR5k5u27d1w2tpewHaEK%26pid%3DApi&f=1', name: 'Crane' }, { pic: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.rtGHltdOrIUqdNXqw-vh6wHaFj%26pid%3DApi&f=1', name: 'Eagle' }, { pic: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.tyfd9yJEQLEWLTKY-Pv2EwHaEK%26pid%3DApi&f=1', name: 'Eagle' }];
 
   /// HOOKS WILL BE HERE FOR DB CALLS
 
@@ -12,16 +12,20 @@ const Profile = (props) => {
       <div className='profile-section'>
         <h3>Profile info:</h3>
         <div className="profile-info">
-          <Avatar size={75} color='blue' />
+          <Avatar size={75} color='#C8994D' />
           <div className='user-info container'>
-
+            Mr. Raymonds
+            <span class="badge rounded-pill bg-info text-dark">Frequent Flyer</span>
+            <span class="badge rounded-pill bg-success">Migrant</span>
           </div>
         </div>
       </div>
       <div className='social-section'>
         <h3>Social stats:</h3>
         <div className="social-view">
-          <h2>Need to decide what stats matter here</h2>
+          <h6>Most birds<span class="badge bg-warning">1</span></h6>
+          <h6>Frequent Flyer <span class="badge bg-primary">3</span></h6>
+          <h6>Migrant <span class="badge bg-secondary">6</span></h6>
         </div>
       </div>
       <div className='bird-nest-section'>
@@ -30,7 +34,8 @@ const Profile = (props) => {
           {fakeData.map((bird, i) => {
             return (
               <div className='bird-nest-item'>
-                <Avatar size={75} pic={bird.pic} name={bird.name} color='red' />
+                {/* <Avatar size={75} pic={bird.pic} name={bird.name} color='red' /> */}
+                <img className='rounded-3' style={{ width: 75, height: 75 }} src={bird.pic} alt="" />
               </div>
             );
           })}
--- client/components/Shared/Avatar.jsx
+++ client/components/Shared/Avatar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Avatar = (props) => {
   return (
-    <div className='rounded-circle' style={{ width: props.size, height: props.size, backgroundColor: props.color }}>
+    <div className='Avatar rounded-circle' style={{ width: props.size, height: props.size, backgroundColor: props.color }}>
       <img style={{ width: 75, height: 75 }} src={require('../../../public/assets/crane.svg')}></img>
     </div >
   );
